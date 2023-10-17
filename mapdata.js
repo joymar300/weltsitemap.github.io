db5bc248485ea86c2472d2c8e8175f357b9e1d69
joymar300 <74611985+joymar300@users.noreply.github.com>
2023-10-16 23:35:02 -0500

    a

---
+++ mapdata.js
@@ -0,0 +1,310 @@
+var simplemaps_countrymap_mapdata = {
+  main_settings: {
+    //General settings
+    width: "950", //'700' or 'responsive'
+    background_color: "#transparent",
+    background_transparent: "yes",
+    height: "1250",
+    //State defaults
+    state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#B8B7B7",
+    state_width:"100",
+    state_height:"50",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_height:"10",
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "16px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "yes",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    COL1283: {
+      name: "Amazonas",
+      description: 'Amazonas es el departamento más meridional de Colombia, ubicado en la selva amazónica. Es conocido por su rica biodiversidad y culturas indígenas.',
+
+      color:'#CB574A',
+      url:"https://youtu.be/ms6Bpl25pfs?si=1Io52anri2Q3t5pt"
+    },
+    COL1314: {
+      name: "Antioquia",
+      description: 'Antioquia es un departamento en el noroeste de Colombia, conocido por sus vibrantes ciudades, impresionantes paisajes y producción de café.',
+
+      color:'#AEC64E',
+      url:"https://youtu.be/PHzGBM518tA?si=MjF3kievJEB25ks6"
+    },
+    COL1315: {
+      name: "Boyacá",
+      description: 'Boyacá es un departamento en la región andina de Colombia, famoso por sus pueblos coloniales, diversa geografía y patrimonio cultural.',
+
+      color:'#D48A43',
+      url:"https://youtu.be/Ek5xi74AyuY?si=4QuoMhIrf1DAdPnC"
+    },
+    COL1316: {
+      name: "Córdoba",
+      description: 'Córdoba es un departamento en el norte de Colombia, conocido por sus playas, música vallenata y rica historia cultural.',
+
+      color:'#F1B600'
+    },
+    COL1317: {
+      name: "Santander",
+      description: 'Santander es un departamento en el noreste de Colombia, famoso por sus cañones, parques naturales y rica gastronomía.',
+
+      color:'#C666E4',
+      url:"https://youtu.be/39l0SG7Az-8?si=Bt3CzjglkLbVnuF3"
+    },
+    COL1318: {
+      name: "La Guajira",
+      description: 'La Guajira es un departamento en el norte de Colombia, conocido por su desierto, playas vírgenes y cultura indígena Wayuu.',
+      color:'#C666E4',
+      url: "https://youtu.be/-M-JVxCDXP8?si=8qVR_7WDcj_feZm4"
+    },
+    COL1342: {
+      name: "San Andrés y Providencia",
+      description: 'San Andrés y Providencia es un departamento insular en el Caribe colombiano, famoso por sus playas de arena blanca y arrecifes de coral.',
+
+      color:'#71AB6E',
+      url:"https://youtu.be/W7mKcY-N4T4?si=iGTFM8p4ZKGpl0W3"
+    },
+    COL1397: {
+      name: "Caldas",
+      description: 'Caldas es un departamento en la región andina de Colombia, conocido por sus aguas termales y paisajes montañosos.',
+
+      color:'#6CA767',
+      url:"https://youtu.be/qdoPiRGViqY?si=7BDN9QuGY8ZBJCII"
+    },
+    COL1398: {
+      name: "Cundinamarca",
+      description: 'Cundinamarca es un departamento en la región andina de Colombia, que rodea la capital, Bogotá, y es conocido por su patrimonio histórico y natural.',
+
+      color:'#EBB604',
+      ulr:"https://youtu.be/iIbuWo_lpKE?si=5EHZt_v9ubloCmBS"
+    },
+    COL1399: {
+      name: "Bogotá",
+      description: 'Bogotá D.C. es la capital de Colombia y una entidad territorial especial, conocida por su rica historia, cultura y arquitectura.',
+      color:'#C666E4'
+    },
+    COL1400: {
+      name: "Quindío",
+      description: 'Quindío es un departamento en la región andina de Colombia, famoso por sus paisajes cafeteros y arquitectura colonial.',
+      color:'#B1BE4A',
+      url:"https://youtu.be/uWGH7qxtVdA?si=I37aDQ4e8k8MCkAt"
+    },
+    COL1401: {
+      name: "Risaralda",
+      description: 'Risaralda es un departamento en la región andina de Colombia, conocido por su biodiversidad y producción de café.',
+      color:'#C25A4F',
+      url:"https://youtu.be/aM3qa1HpluI?si=C0R7r8IudJrDiD3W"
+    },
+    COL1402: {
+      name: "Tolima",
+      description: 'Tolima es un departamento en la región andina de Colombia, famoso por su paisaje montañoso y cultura folclórica.',
+
+      color:'#D78A3A',
+      url:"https://youtu.be/VmyL3l4j7S0?si=reI30mlBaIkM608g"
+    },
+    COL1403: {
+      name: "Caquetá",
+      description: 'Caquetá es un departamento en el sur de Colombia, conocido por sus paisajes naturales y riqueza en biodiversidad.',
+
+      color:'#F1B600',
+      url:"https://youtu.be/qdoPiRGViqY?si=7BDN9QuGY8ZBJCII"
+    },
+    COL1404: {
+      name: "Cauca",
+      description: 'Cauca es un departamento en el suroeste de Colombia, famoso por sus riquezas arqueológicas y paisajes naturales.',
+
+      color:'#CB574A',
+      url:"https://youtu.be/iXm39eU1Stc?si=2xWci1PJxWZnZN61"
+
+    },
+    COL1405: {
+      name: "Huila",
+      description: 'Huila es un departamento en el suroeste de Colombia, conocido por su geografía montañosa y parques naturales.',
+
+      color:'#C666E4'
+    },
+    COL1406: {
+      name: "Nariño",
+      description: 'Nariño es un departamento en el suroeste de Colombia, famoso por su biodiversidad y riqueza cultural.',
+   
+      color:'#D7873E',
+      url: "https://youtu.be/Qh7PW1XOmOg?si=6aqTvKvE8x67HrAb"
+    },
+    COL1407: {
+      name: "Putumayo",
+      description: 'Putumayo es un departamento en el sur de Colombia, conocido por sus selvas tropicales y culturas indígenas.',
+
+      color:'#6EA669',
+      url:"https://youtu.be/HbhivX9Nomk?si=E2JiQnCHEYScAJ1C"
+    },
+    COL1408: {
+      name: "Valle del Cauca",
+      description: 'Valle del Cauca es un departamento en el suroeste de Colombia, famoso por su gastronomía, playas y cultura.',
+
+      color:'#EEB503',
+      url:"https://youtu.be/eQJQtmzv92Y?si=pjnsLNv4GWMbPfUh"
+    },
+    COL1412: {
+      name: "Atlántico",
+      description: 'Atlántico es un departamento en la costa norte de Colombia, conocido por sus playas y su importante puerto en Barranquilla.',
+
+      color:"#E5B912",
+      url:"https://youtu.be/fF8NApcYgdc?si=_cs1qcClRFJb3K4q"
+    },
+    COL1413: {
+      name: "Bolívar",
+      description: 'Bolívar es un departamento en la costa norte de Colombia, famoso por su historia colonial y sus playas caribeñas.',
+
+      color:'#CA5647',
+      url:"https://youtu.be/v_NZlqOO6k8?si=B51ZLKF2du3H1BBW"
+    },
+    COL1414: {
+      name: "Cesar",
+      description: 'Cesar es un departamento en el noreste de Colombia, conocido por su producción de carbón y su riqueza cultural.',
+
+      color:'#D8893E',
+      url:"https://youtu.be/00_I9s3_ADc?si=mSUtipDBaBDHt5of"
+    },
+    COL1415: {
+      name: "Chocó",
+      description: 'Chocó es un departamento en el noroeste de Colombia, conocido por su biodiversidad y culturas indígenas.',
+
+      color:'#C666E4',
+      url:"https://youtu.be/i_vEJpG56NY?si=O1SocuJObC_FU1Ok"
+    },
+    COL1416: {
+      name: "Magdalena",
+      description: 'Magdalena es un departamento en la costa norte de Colombia, famoso por sus playas caribeñas y el río Magdalena.',
+
+      color:'#AEC64E',
+      url:"https://youtu.be/uDV9Eu2SsBA?si=vi7AE0DX6J2QS_Ao"
+    },
+    COL1417: {
+      name: "Sucre",
+      description: 'Sucre es un departamento en la costa norte de Colombia, conocido por su rica historia colonial y playas vírgenes.',
+ 
+      color:'#C666E4',
+      url:"https://youtu.be/UU398TK6siI?si=MLasdr9WGzluEG9l"
+    },
+    COL1420: {
+      name: "Arauca",
+      description: 'Arauca es un departamento en el este de Colombia, conocido por su riqueza en petróleo y su cultura llanera.',
+
+      color:'#ADC74C',
+      url:"https://youtu.be/hVbdsM1jHXA?si=5mdyLm1SIP6U78XU"
+    },
+    COL1421: {
+      name: "Norte de Santander",
+      description: 'Norte de Santander es un departamento en el noreste de Colombia, conocido por sus montañas y la ciudad de Cúcuta.',
+
+      color:'#6CA765',
+      url:"https://youtu.be/aduRLUql_Bw?si=jDN-PDo_OWHgk662"
+    },
+    COL1422: {
+      name: "Casanare",
+      description: 'Casanare es un departamento en el este de Colombia, famoso por su ganadería y paisajes llaneros.',
+
+      color:'#6EA667',
+      url:"https://youtu.be/JtEi2eB-5vI?si=WpKEuVHNlTBCTYYu"
+    },
+    COL1423: {
+      name: "Guaviare",
+      description: 'Guaviare es un departamento en el sur de Colombia, conocido por sus ríos y selvas tropicales.',
+
+      color:'#6EA667',
+      url:"https://youtu.be/XVxEiucWMY0?si=WfDbaeQKohvyqWjd"
+    },
+    COL1424: {
+      name: "Guainía",
+      description: 'Guainía es un departamento en el sureste de Colombia, ubicado en la región de la Amazonía. Conocido por sus ríos y selvas vírgenes, es ideal para el turismo de naturaleza y aventura.',
+
+      color:'#C666E4',
+      url:"https://youtu.be/WrDvRtu-xUM?si=RNy9ShHJFqlzMNAE"
+    },
+    COL1425: {
+      name: "Meta",
+      description: 'Meta es un departamento en la región de los Llanos de Colombia, famoso por sus extensas llanuras, ríos y actividades agropecuarias.',
+
+      color:'#AEC64E',
+      url:"https://youtu.be/XsmiOsSZFFo?si=50jyF9Q3jnOf66_1"
+    },
+    COL1426: {
+      name: "Vaupés",
+      description: 'Vaupés es un departamento en el sur de Colombia, conocido por su paisaje de ríos y selvas vírgenes.',
+
+      color:'#D7873E',
+      url:"https://youtu.be/xcEB5-8mQTI?si=yENaeCfVdTcfoIws"
+    },
+    COL1427: {
+      name: "Vichada",
+      description: 'Vichada es un departamento en el este de Colombia, conocido por sus parques naturales y biodiversidad.',
+
+      color:'#F1B600',
+      url:"https://youtu.be/stw9enc7KoA?si=kxImaBYJtKuWgOo1"
+    }
+  },
+  locations: {
+  },
+  labels: {},
+  legend: {
+    entries: []
+  },
+  regions: {}
+};
